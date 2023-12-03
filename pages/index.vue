fb0cbd0f61d311450ded37aecfdf377e336bd2b2
Hypnos <47209391+Jace254@users.noreply.github.com>
2023-12-03 22:36:10 +0300

    chore: setup eslint

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-const {x, y} = useMouse()
 const color = useColorMode()
+
 function toggleDark() {
-    color.value = color.value === 'dark' ? 'light' : 'dark'
+    color.value = color.value === 'dark' 
+    ? 'light' 
+    : 'dark'
 }
 </script>
 
