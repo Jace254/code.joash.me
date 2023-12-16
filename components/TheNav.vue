0846a3051360e07632e9a49014a6873a7686bf83
Hypnos <joashmacer@gmail.com>
2023-12-16 21:22:44 +0300

    chore: add github link button on nav

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,11 +1,18 @@
 <template>
-  <header py-1 bg-background:75 backdrop-blur border-b border-border mb="-1px" sticky top-0 z-50>
-    <nav mx-auto px-4 sm:px-6 lg:px-8 max-w-8xl flex items-center justify-between gap-3 h-full >
+  <header py-3 bg-background:75 backdrop-blur border-b border-border mb="-1px" sticky top-0 z-50>
+    <nav mx-auto px-4 sm:px-6 lg:px-8 max-w-8xl flex items-center gap-3 h-full>
       <NuxtLink to="/" title="CodeGround">
         <Logo h-2em />
       </NuxtLink>
-      <!-- <div flex-auto /> -->
+      <div flex-auto />
       <ColorSchemeToggle />
+      <NuxtLink
+        href="https://github.com/Jace254/code.joash.me"
+        rounded p-2
+        hover="bg-active"
+      >
+        <div i-bi-github />
+      </NuxtLink>
     </nav>
   </header>
 </template>
