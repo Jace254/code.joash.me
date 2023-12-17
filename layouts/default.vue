2806fa373ae1d494d755ca577ab8ab530abe7c98
Hypnos <joashmacer@gmail.com>
2023-12-17 09:44:27 +0300

    style: change docs styling for scroll

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
-  <div h-100dvh h-screen w-screen of-hidden grid="~ rows-[max-content_1fr]" bg-base>
+  <div
+    h-100dvh w-screen of-hidden
+    grid="~ rows-[max-content_1fr]" bg-base
+  >
     <TheNav />
     <slot />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div grid grid-cols="[1fr_2fr]" h-full of-hidden max-h-full>
-    <div b-r b-base p4>
+  <div
+    grid grid-cols="[1fr_2fr]"
+    h-full of-hidden max-h-full
+  >
+    <div b-r b-base>
       <Chat />
     </div>
     <Playground />
