c99e1d98d62703484cb3033db584588850011a99
Hypnos <joashmacer@gmail.com>
2023-12-17 09:43:54 +0300

    feat: support mounting nested folder structure to wc

--- composables/webContainer.ts
+++ composables/webContainer.ts
@@ -1,4 +1,4 @@
-import { WebContainer } from '@webcontainer/api'
+import { type FileSystemTree, WebContainer } from '@webcontainer/api'
 
 let _webcontainerInstance: WebContainer
 
@@ -6,4 +6,43 @@ export async function useWebContainer() {
   if (!_webcontainerInstance)
     _webcontainerInstance = await WebContainer.boot()
   return _webcontainerInstance
+}
+
+export function globToWebContainerFs(
+  prefix: string,
+  rawFiles: Record<string, string>,
+) {
+  const files = Object.fromEntries(
+    Object.entries(rawFiles).map(([path, content]) => {
+      return [path.replace(prefix, ''), {
+        file: {
+          contents: content,
+        },
+      }]
+    }),
+  )
+
+  const tree: FileSystemTree = {}
+
+  for (const [path, file] of Object.entries(files)) {
+    if (!path.includes('/')) { tree[path] = file }
+    else {
+      const segments = path.split('/')
+      const filename = segments.pop()!
+      let current = tree
+      for (const dir of segments) {
+        if (!current[dir]) {
+          current[dir] = {
+            directory: {},
+          }
+        }
+        const node = current[dir]
+        if (!('directory' in node))
+          throw new Error('Expected directory but found file')
+        current = node.directory
+      }
+      current[filename] = file
+    }
+  }
+  return tree
 }
