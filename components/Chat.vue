4e534054f45d3bc41cd5b53708b674aab1430bed
Hypnos <joashmacer@gmail.com>
2023-12-17 04:03:01 +0300

    feat: add send button and fix parsedMarkdown for empty completiion

--- components/Chat.vue
+++ components/Chat.vue
@@ -5,22 +5,27 @@ import { Input } from '@/components/ui/input'
 
 const parsedMarkdown = ref<Record<string, any> | undefined>(undefined)
 
-const { messages, input, handleSubmit } = useChat()
+const { messages, input, handleSubmit, isLoading } = useChat()
 const data = ref<string>('')
 
 watch(() => messages.value, async (m) => {
   data.value = m[m.length - 1] && m[m.length - 1].role !== 'user' ? m[m.length - 1].content : ''
-  parsedMarkdown.value = await markdownParser.parse!('', data.value, {})
+  if (data.value !== '')
+    parsedMarkdown.value = await markdownParser.parse!('', data.value, {})
 })
 </script>
 
 <template>
-  <form @submit="handleSubmit">
+  <form flex w-full @submit="handleSubmit">
     <Input
       v-model="input"
-      class="w-full  p-2 mb-8"
+      class="w-full p-2 mb-8 mr-2 bg-faded"
       placeholder="Ask me anything related to node..."
     />
+    <UiButton :disabled="isLoading" @click="handleSubmit">
+      <div v-if="isLoading" i-svg-spinners-270-ring/>
+      <div v-if="!isLoading" i-zondicons-send/>
+    </UiButton>
   </form>
   <article prose>
     <div class="whitespace-pre-wrap">
